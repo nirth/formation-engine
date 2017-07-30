2a9a0c71224b7b48720e480b3d33e31d990d25b6
David Sergey <nirth.furzahad@gmail.com>
2017-07-30 12:07:01 +0100

    Optimising building

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+const flow = require('rollup-plugin-flow')
+const uglify = require('rollup-plugin-uglify')
 
 export default {
   entry: 'src/index.js',
   format: 'cjs',
   dest: './lib/lib.js',
+  plugins: [
+    flow({pretty: true}),
+  ],
   external: [
     'ramda',
   ],
