e11192b6131171feb57fbab35c4f1c53e7ec88e2
David Sergey <nirth.furzahad@gmail.com>
2017-07-31 15:31:47 +0100

    Updated ESLint settings.
     * Organized by cosmetic and complexity.
     * Added rules to make sure that code is not too complex via limit cyclomatic complexity, max params, max statements.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,23 @@ module.exports = {
   "parser": "babel-eslint",
   "extends": ["airbnb-base", "plugin:flowtype/recommended"],
   "rules": {
-    "arrow-parens": ["error", "always"],
-    "object-curly-spacing": ["error", "never"],
+    // Enforcing complexity of the functions by:
+    // limiting params, number of statements per function, cyclomatic complexity and callbacks.
+    "max-statements": ["error", 7],
+    "max-nested-callbacks": ["error", 3],
+    "max-params": ["error", 5],
+    "complexity": ["error", 10],
+    "max-len": ["warn", 120, {
+      "ignoreComments": true,
+      "ignoreTrailingComments": true,
+      "ignoreUrls": true,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true,
+    }],
+    // Overall code style
     "semi": ["error", "never"],
-    "max-len": ["warn", 120],
+    "object-curly-spacing": ["error", "never"],
+    "arrow-parens": ["error", "always"],
     "no-trailing-spaces": ["error", {"skipBlankLines": true}],
     "comma-dangle": ["error", {
       "arrays": "always-multiline",
@@ -18,7 +31,6 @@ module.exports = {
   "env": {
     "browser": true,
     "node": true,
-    "mocha": true,
     "es6": true
   },
   "plugins": [
