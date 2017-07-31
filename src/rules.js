c93a65fff57f716775c22babea540251d74f5fbd
David Sergey <nirth.furzahad@gmail.com>
2017-07-31 16:28:35 +0100

    Refactoring / Modifying the way `shouldRuleBeInvoked`

--- src/rules.js
+++ src/rules.js
@@ -9,14 +9,22 @@ export const createRule = (validations: Validation[], required: boolean = false,
   defaultValue,
 })
 
+const hasNoValidations = (validations) => (!Array.isArray(validations) || validations.length === 0)
+const valueNotRequiredAndSameAsDefault = (value: Value, defaultValue: Value, required: boolean) => (
+  !required && value === defaultValue
+)
+
 const shouldRuleBeInvoked = (rule: Rule, value: Value): boolean => {
   // TODO: This function is provisionaly naive implementation.
-  if (rule.required && value === rule.defaultValue) {
-    // Abort validation if field if field is _not_ required and value _is_ defaultValue
+
+  if (valueNotRequiredAndSameAsDefault(value, rule.defaultValue, rule.required) || // Abort validation if field if field is _not_ required and value _is_ defaultValue
+      hasNoValidations(rule.validations) // Rule has no validations, there is nothing to do with it, even if it is required.
+  ) {
     return false
   }
+
   return true
-})
+}
 
 const runRule = (rule: Rule, value: Value): Result => {
   if (!shouldRuleBeInvoked(rule, value)) {
@@ -27,7 +35,8 @@ const runRule = (rule: Rule, value: Value): Result => {
     // $FlowFixMe: I‘m right, flow wrong in this case.
     .map((validation: Validation): ?Message => validation(value))
     .filter((maybeMessage: ?Message): boolean => typeof maybeMessage === 'string'))
-})
+}
+
 const curriedShouldRuleBeInvoked = curry(shouldRuleBeInvoked)
 const curriedRunRule = curry(runRule)
 
