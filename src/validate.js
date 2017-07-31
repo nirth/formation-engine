d9fb29c3effe840d70305f3b188025632e665d0b
David Sergey <nirth.furzahad@gmail.com>
2017-07-31 16:27:39 +0100

    Added new library API function - `validateSingleField`

--- src/validate.js
+++ src/validate.js
@@ -28,3 +28,18 @@ const validate = (rules: Rules, values: Values): Results => ramdaValues(
   )
 
 export default curry(validate)
+/**
+ * Validates single field against set of `rules`.
+ *
+ * @param {Object | Rules} rules - Set of rules that used by form to validate.
+ * @param {string} field - Indicates field to be validated
+ * @param {Object | Values} values - Values of the individual inputs that should be validated.
+ * @returns {Result} - Results of validation.
+ */
+const validateSingleField = (rules: Rules, field: string, values: Values): Result => {
+  // We are passing entire subset of rules instead of a single rule for two reasons:
+  // * To maintain API to be somewhat similar to `validate`.
+  // * As a future
+  console.log('validateSingleField', rules, field, values)
+  return runRule(rules[field], values[field])
+}
